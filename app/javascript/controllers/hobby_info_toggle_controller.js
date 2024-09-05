346a1432176c8f5ed32e7aabb88eeba50c94ee22
michael stump <maikerustump@gmail.com>
2024-09-06 01:10:22 +0900

    assigned styling to about me after buttons pressed

--- app/javascript/controllers/hobby_info_toggle_controller.js
+++ app/javascript/controllers/hobby_info_toggle_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hobby-info-toggle"
 export default class extends Controller {
-  static targets = [ "button", "mover" ]
+  static targets = [ "button", "mover", "web", "games", "board" ]
 
   connect() {
     // console.log("Hello from the hobby info toggle controller");
@@ -26,12 +26,24 @@ export default class extends Controller {
     const currentHobby = event.currentTarget.dataset.hobby;
     if(currentHobby === "game"){
       this.moverTarget.classList.add("game");
+
+      this.gamesTarget.querySelector("a").classList.add("games");
+      this.webTarget.querySelector("a").classList.remove("web");
+      this.boardTarget.querySelector("a").classList.remove("board-games");
     }
     if(currentHobby === "web"){
       this.moverTarget.classList.add("web");
+
+      this.webTarget.querySelector("a").classList.add("web");
+      this.gamesTarget.querySelector("a").classList.remove("games");
+      this.boardTarget.querySelector("a").classList.remove("board-games");
     }
     if(currentHobby === "board"){
       this.moverTarget.classList.add("board-game");
+
+      this.boardTarget.querySelector("a").classList.add("board-games");
+      this.webTarget.querySelector("a").classList.remove("web");
+      this.gamesTarget.querySelector("a").classList.remove("games");
     }
   }
 }
