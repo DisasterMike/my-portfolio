d2be9c721c7a4983845ef6f79049303c993beed9
michael stump <maikerustump@gmail.com>
2024-09-05 21:32:58 +0900

    toggle animations fully set up

--- app/javascript/controllers/hobby_info_toggle_controller.js
+++ app/javascript/controllers/hobby_info_toggle_controller.js
@@ -17,8 +17,21 @@ export default class extends Controller {
     // Add active class to current hovered button
     event.currentTarget.classList.add("active");
 
-    // Make text change under the buttons
-    console.log(this.moverTarget);
-    this.moverTarget.classList.add("board-game")
+    // Remove all hobby classes from text divs
+    this.moverTarget.classList.remove("game");
+    this.moverTarget.classList.remove("web");
+    this.moverTarget.classList.remove("board-game");
+
+    // Add hobby class to the text div
+    const currentHobby = event.currentTarget.dataset.hobby;
+    if(currentHobby === "game"){
+      this.moverTarget.classList.add("game");
+    }
+    if(currentHobby === "web"){
+      this.moverTarget.classList.add("web");
+    }
+    if(currentHobby === "board"){
+      this.moverTarget.classList.add("board-game");
+    }
   }
 }
