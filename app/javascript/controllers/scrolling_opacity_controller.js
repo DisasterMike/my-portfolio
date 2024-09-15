e69cf46ec933a5e49dd16eb0fed6593459aec05e
michael stump <maikerustump@gmail.com>
2024-09-15 13:29:41 +0900

    picture row basic styling

--- app/javascript/controllers/scrolling_opacity_controller.js
+++ app/javascript/controllers/scrolling_opacity_controller.js
@@ -5,9 +5,9 @@ export default class extends Controller {
   static targets = [ "scrollWatcher", "project" ]
 
   connect() {
-    console.log("Hello from the scrolling opacity controller");
-    console.log(this.scrollWatcherTarget);
-    console.log(this.projectTarget);
+    // console.log("Hello from the scrolling opacity controller");
+    // console.log(this.scrollWatcherTarget);
+    // console.log(this.projectTarget);
 
     const navObserver = new IntersectionObserver((entries) => {
       // this.projectTarget.classList.toggle("opacity-full", entries[0].isIntersecting); // second parameter makes it so the toggle will only remove if false, or add if true
